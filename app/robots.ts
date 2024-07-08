59bbb74c72118a06b81d5bae92a88b9688ab9c81
sukronsabari <sukronsabari11@gmail.com>
2024-07-08 10:58:07 +0800

    rename robot to robots

--- app/robots.ts
+++ app/robots.ts
@@ -1,12 +1,12 @@
 import { MetadataRoute } from "next";
 
-const baseUrl = process.env.NEXT_PUBLIC_SERVER_URL!;
 export default function robots(): MetadataRoute.Robots {
+  const baseUrl = process.env.NEXT_PUBLIC_SERVER_URL!;
   return {
     rules: {
       userAgent: "*",
       allow: "/",
-      disallow: ["/api/", "/admin/", "/cart/"],
+      disallow: ["/api/", "/admin/", "/cart"],
     },
     sitemap: `${baseUrl}/sitemap.xml`,
   };
